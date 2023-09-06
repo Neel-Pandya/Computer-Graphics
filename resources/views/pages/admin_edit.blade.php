0591308dcae3cdf9902473464d47a65c83c60552
Neel pandya <neelpandya2601@gmail.com>
2023-09-06 11:25:54 +0530

    small change in edit profile admin side

--- resources/views/pages/admin_edit.blade.php
+++ resources/views/pages/admin_edit.blade.php
@@ -40,61 +40,59 @@
 
     @foreach ($admin_data as $admin)
     @endforeach
-    <img src="{{ URL::to('/') }}/images/admin/{{ $admin->admin_profile }}" class="img-fluid img-sm" />
-
-    <form action="{{ route('admin.update') }}" method="POST" enctype="multipart/form-data">
-        @csrf
-        <div class="row">
-            <div class="mt-4 col-lg-6 col-md-6 col-sm-12">
-                <label for="" class="form-label">Admin name</label>
-                <input type="text" name="admin_name" id="" class="form-control" placeholder="Enter Admin name"
-                    value="{{ $admin->admin_name }}">
-                <span class="text-danger">
-                    @error('admin_name')
-                        {{ $message }}
-                    @enderror
-                </span>
-            </div>
-
-            <div class="mt-4 col-lg-6 col-md-6 col-sm-12">
-                <label for="" class="form-label">Admin email</label>
-                <input type="email" name="admin_email" id="" class="form-control" placeholder="Enter Admin email"
-                    value="{{ $admin->admin_email }}" readonly>
-
-                <span class="text-danger">
-                    @error('admin_email')
-                        {{ $message }}
-                    @enderror
-                </span>
-            </div>
-
-
-
-            <div class="mt-4 col-lg-12 col-md-12 col-sm-12 ">
-                <label for="" class="form-label">Choose Profile</label>
-                <input type="file" name="profile" id="" class="form-control">
-                <span class="text-danger">
-                    @error('profile')
-                        {{ $message }}
-                    @enderror
-                </span>
-            </div>
-
-
-
-
-            <div class="mt-4 col-lg-12 col-md-12 col-sm-12 text-center">
-                <input type="submit" value="Submit" class="btn btn-primary text-center">
-            </div>
-
-
-
-
-
+    <div class="row">
+        <div class="col-lg-4 col-md-6 col-sm-12 mt-4">
+            <img src="{{ URL::to('/') }}/images/admin/{{ $admin->admin_profile }}" class="img-fluid" width="100%" />
         </div>
 
+        <form action="{{ route('admin.update') }}" method="POST" enctype="multipart/form-data" class="col-lg-8 col-md-6 col-sm-12">
+            @csrf
+                
+                    <label for="" class="form-label mt-4">Admin name</label>
+                    <input type="text" name="admin_name" id="" class="form-control"
+                        placeholder="Enter Admin name" value="{{ $admin->admin_name }}">
+                    <span class="text-danger">
+                        @error('admin_name')
+                            {{ $message }}
+                        @enderror
+                    </span>
+                
+                    <label for="" class="form-label mt-4">Admin email</label>
+                    <input type="email" name="admin_email" id="" class="form-control"
+                        placeholder="Enter Admin email" value="{{ $admin->admin_email }}" readonly>
 
-    </form>
+                    <span class="text-danger">
+                        @error('admin_email')
+                            {{ $message }}
+                        @enderror
+                    </span>
+
+
+
+                    <label for="" class="form-label mt-4">Choose Profile</label>
+                    <input type="file" name="profile" id="" class="form-control">
+                    <span class="text-danger">
+                        @error('profile')
+                            {{ $message }}
+                        @enderror
+                    </span>
+
+
+
+
+
+                <div class="mt-4 col-lg-12 col-md-12 col-sm-12 text-center">
+                    <input type="submit" value="Submit" class="btn btn-primary text-center">
+                </div>
+
+
+
+
+
+            </div>
+
+
+        </form>
 
     </div>
     </div>
