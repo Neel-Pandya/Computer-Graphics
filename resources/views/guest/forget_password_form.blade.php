c7391eca7bfa24df13be937d8efe4f82624a7e66
Neel pandya <neelpandya2601@gmail.com>
2023-09-24 14:17:32 +0530

    Field added in forget password form

--- resources/views/guest/forget_password_form.blade.php
+++ resources/views/guest/forget_password_form.blade.php
@@ -13,84 +13,20 @@
     <div class="container mt-4 col-6">
         <input type="checkbox" id="check">
         <div class="login form">
-            <header class="logo-name" style="font-family: cursive">Register</header>
+            <header class="logo-name" style="font-family: cursive">Forget Password</header>
 
-            <form method="POST" enctype="multipart/form-data" action="{{ route('guest.confirm.register') }}">
+            <form method="POST" enctype="multipart/form-data" action="">
                 @csrf
                 <div class="row">
-                    <div class="col-lg-6 col-md-6 col-sm-12 mt-4">
-                        <input type="text" placeholder="Enter your name" name="customer_name" value="{{ old('customer_name') }}">
+                    <div class="col-lg-12 col-md-12 col-sm-12 mt-4">
+                        <input type="email" placeholder="Enter your email" name="em" required>
                         <span class="text-danger">
                             @error('customer_name')
                                 {{ $message }}
                             @enderror
                         </span>
                     </div>
-                    <div class="col-lg-6 col-md-6 col-sm-12 mt-4">
-
-                        <input type="email" placeholder="Enter Your email" name="customer_email" value="{{ old('customer_email') }}">
-                        <span class="text-danger">
-                            @error('customer_email')
-                                {{ $message }}
-                            @enderror
-                        </span>
-                    </div>
-                    <div class="col-lg-6 col-md-6 col-sm-12 mt-4">
-
-                        <input type="password" placeholder="Enter Your Password" name="customer_password" value="{{ old('customer_password') }}">
-                        <span class="text-danger">
-                            @error('customer_password')
-                                {{ $message }}
-                            @enderror
-                        </span>
-                    </div>
-                    <div class="col-lg-6 col-md-6 col-sm-12 mt-4">
-
-                        <input type="password" placeholder="Confirm password" name="customer_password_confirmation" value="{{ old('customer_password_confirmation') }}">
-                        <span class="text-danger">
-                            @error('customer_password_confirmation')
-                                {{ $message }}
-                            @enderror
-                        </span>
-                    </div>
-                    <div class="col-lg-6 col-md-6 col-sm-12 mt-4">
-
-                        <select name="customer_gender" id="" class="form-select form-control">
-                            <option value="">Select gender</option>
-                            @php
-                                $arr = ['Male', 'Female'];
-                            @endphp
-                            @foreach ($arr as $item)
-                                <option value="{{ $item }}" {{ old('customer_gender') == $item ? 'selected' : '' }}>{{ $item }}</option>
-                            @endforeach
-                        </select>
-                        <span class="text-danger">
-                            @error('customer_gender')
-                                {{ $message }}
-                            @enderror
-                        </span>
-                    </div>
-
-
-                    <div class="col-lg-6 col-md-6 col-sm-12 mt-4">
-
-                        <input type="number" placeholder="Enter Your Number" name="customer_number" value="{{ old('customer_number') }}">
-                        <span class="text-danger">
-                            @error('customer_number')
-                                {{ $message }}
-                            @enderror
-                        </span>
-                    </div>
-                    <div class="col-lg-12 col-md-12 col-sm-12 col-mt-4">
-                        <input type="file" name="pic" id="">
-                        <span class="text-danger">
-                            @error('pic')
-                                {{ $message }}
-                            @enderror
-                        </span>
-                    </div>
-
-
+                  
                     <input type="submit" class="button" value="Register">
                 </div>
             </form>
